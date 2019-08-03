0e04ed4b1e979919008a5c1df0c9bd050dd189ed
Andrew Smith <codingmba@gmail.com>
2019-08-03 18:42:23 -0400

    initial commit

---
+++ src/shared/Header.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div>
+      <nav className='navbar navbar-dark navbar-expand-lg'>
+        <div className='container'>
+          <a className='navbar-brand' href=''>Royal Palm Nanny</a>
+          <form className='form-inline my-2 my-lg-0'>
+            <input className='form-control mr-sm-2 rpn-search' type='search' placeholder='Search by Name' aria-label='Search'/>
+            <button className='btn btn-outline-success my-2 my-sm-0 btn-rpn-search' type='submit'>Search</button>
+          </form>
+          <button className='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarNavAltMarkup' aria-controls='navbarNavAltMarkup' aria-expanded='false' aria-label='Toggle navigation'>
+            <span className='navbar-toggler-icon'></span>
+          </button>
+          <div className='collapse navbar-collapse' id='navbarNavAltMarkup'>
+            <div className='navbar-nav ml-auto'>
+              <a className='nav-item nav-link active' href=''>Login <span className='sr-only'>(current)</span></a>
+              <a className='nav-item nav-link' href=''>Register</a>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
