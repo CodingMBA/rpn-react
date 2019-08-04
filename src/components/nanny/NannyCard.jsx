3af9c5cc304ddf268e866599c8b03379a96c14fe
Andrew Smith <codingmba@gmail.com>
2019-08-04 12:01:44 -0400

    adds new editor and linting configs, replaces anchors with buttons, changes colors to vue colors, changes nanny listing placeholder text.

--- src/components/nanny/NannyCard.jsx
+++ src/components/nanny/NannyCard.jsx
@@ -4,18 +4,14 @@ function NannyCard() {
   return (
     <div className="col-md-3 col-xs-6">
       <div className="card rpn-card">
-        <img
-          className="card-img-top"
-          src="http://via.placeholder.com/350x250"
-          alt=""
-        />
+        <img className="card-img-top" src="http://via.placeholder.com/350x250" alt="" />
         <div className="card-block">
-          <h6 className="card-subtitle">Janice Griffith</h6>
-          <h4 className="card-title">Some nice apartment</h4>
-          <p className="card-text">$240 per Night &#183; Free Cancelation</p>
-          <a href="#" className="card-link">
+          <h6 className="card-subtitle">Naples, FL</h6>
+          <h4 className="card-title">Rebecca Boyce</h4>
+          <p className="card-text">4.5 Stars</p>
+          <button type="button" className="card-link button-link">
             More Info
-          </a>
+          </button>
         </div>
       </div>
     </div>
